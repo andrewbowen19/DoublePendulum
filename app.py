7007735e4815581fe4622622b09644e3197d4f91
andrewbowen19 <atbowen@gmail.com>
2022-01-17 14:21:47 -0500

    Rewrote as DoublePendulum class

--- app.py
+++ app.py
@@ -8,120 +8,180 @@ import copy
 import matplotlib.animation as animation
 from numpy.lib.function_base import average
 
-g = 9.81
-milliSecondDelayBetweenFrames = 10
-stepsPerFrame = 32
-deltaT = float(milliSecondDelayBetweenFrames) * 0.001 / float(stepsPerFrame)
 
-m_1 = 0.1
-l_1 = 0.3
+class DoublePendulum(object):
 
-m_2 = 0.1
-l_2 = 0.3
+    def __init__(self, savefig=False):
+        self.g = 9.81
+        self.milliSecondDelayBetweenFrames = 20
+        self.stepsPerFrame = 32
+        self.deltaT = float(self.milliSecondDelayBetweenFrames) * 0.001 / float(self.stepsPerFrame)
 
-# at t = 0
-theta_1_0 = 2
-theta_2_0 = 2
-thetaDot_1_0 = 2
-thetaDot_2_0 = 2
+        # Setting pendulum arm lens and masses
+        self.m_1 = 0.1
+        self.l_1 = 0.3
 
-u_vector = [theta_1_0, theta_2_0, thetaDot_1_0, thetaDot_2_0]
+        self.m_2 = 0.10000000000000000000001
+        self.l_2 = 0.3
 
-u_vectorTimeSnapshots = []
-def symplecticEulerOneStep():
-    def findThetaDoubleDot_1(theta_1, theta_2, thetaDot_1, thetaDot_2):
-        return (-m_2*l_1*thetaDot_1**2*np.sin(theta_1 - theta_2)*np.cos(theta_1 - theta_2) + m_2*g*np.sin(theta_2)*np.cos(theta_1 - theta_2) - m_2*l_2*thetaDot_2**2*np.sin(theta_1 - theta_2) - (m_1 + m_2)*g*np.sin(theta_1)) / ((m_1 + m_2)*l_1 - m_2*l_1*np.cos(theta_1 - theta_2)**2)
+        # at t = 0
+        self.theta_1_0 = 2.1
+        self.theta_2_0 = 2
+        self.thetaDot_1_0 = 2
+        self.thetaDot_2_0 = 2
 
-    def findThetaDoubleDot_2(theta_1, theta_2, thetaDot_1, thetaDot_2):
-        return (m_2*l_2*thetaDot_2**2*np.sin(theta_1 - theta_2)*np.cos(theta_1 - theta_2) + (m_1 + m_2)*g*np.sin(theta_1)*np.cos(theta_1 - theta_2) + l_1*thetaDot_1**2*np.sin(theta_1-theta_2)*(m_1 + m_2) - g*np.sin(theta_2)*(m_1 + m_2)) / (l_1*(m_1 + m_2) - m_2*l_2*np.cos(theta_1 - theta_2)**2)
+        self.u_vector = [self.theta_1_0, self.theta_2_0, self.thetaDot_1_0, self.thetaDot_2_0]
+        self.u_vectorTimeSnapshots = []
 
-    u_vector[2] += findThetaDoubleDot_1(u_vector[0], u_vector[1], u_vector[2], u_vector[3]) * deltaT
-    u_vector[3] += findThetaDoubleDot_2(u_vector[0], u_vector[1], u_vector[2], u_vector[3]) * deltaT
+        self.radiusOfGraphAxises = 0.5
+        self.backgroundColorRGB = (1, 1, 1)
+        self.lineColor = (0, 0, 0)
 
-    u_vector[0] += u_vector[2] * deltaT
-    u_vector[1] += u_vector[3] * deltaT
 
-    u_vectorTimeSnapshots.append(copy.deepcopy(u_vector))
+        self.f = plt.figure()
+        self.ax = plt.axes(projection='3d')
+        self.ax.set_xlim3d(-self.radiusOfGraphAxises, self.radiusOfGraphAxises)
+        self.ax.set_ylim3d(-self.radiusOfGraphAxises, self.radiusOfGraphAxises)
+        self.ax.set_zlim3d(-self.radiusOfGraphAxises + self.radiusOfGraphAxises*0.23, self.radiusOfGraphAxises - self.radiusOfGraphAxises*0.23)
+        self.ax.view_init(40, -5)
+        # self.ax.set_title('Double Pendulum')
+        self.ax.set_facecolor(self.backgroundColorRGB)
+        self.f.patch.set_facecolor(self.backgroundColorRGB)
+        plt.axis('off')
 
-radiusOfGraphAxises = 0.5
-backgroundColorRGB = (1, 1, 1)
-lineColor = (0, 0, 0)
+        self.line0, = self.ax.plot(0, 0, 0, lw=0.5, color=((0, 0, 1)))
+        self.line1, = self.ax.plot(0, 0, 0, lw=0.5, color=((1, 0, 0)))
+        self.bar0, = self.ax.plot(0, 0, 0, lw=3, color=(self.lineColor))
+        self.bar1, = self.ax.plot(0, 0, 0, lw=3, color=(self.lineColor))
 
-def getAxisCoordinatesOverTimeForParticle(_particle, _axis):
+        # Create animation
+        ani = animation.FuncAnimation(self.f, func=self.animation_frame, frames=np.arange(0, 750, 1), interval=self.milliSecondDelayBetweenFrames)
+        plt.show()
+
+        # print('Animation shown!')
+        if savefig:
+            ani.save('doublePendulum.gif')
+
+    # @staticmethod
+    def findThetaDoubleDot_1(self, theta_1, theta_2, thetaDot_1, thetaDot_2):
+        return (-self.m_2*self.l_1*thetaDot_1**2*np.sin(theta_1 - theta_2)*np.cos(theta_1 - theta_2) + self.m_2*self.g*np.sin(theta_2)*np.cos(theta_1 - theta_2) - self.m_2*self.l_2*thetaDot_2**2*np.sin(theta_1 - theta_2) - (self.m_1 + self.m_2)*self.g*np.sin(theta_1)) / ((self.m_1 + self.m_2)*self.l_1 - self.m_2*self.l_1*np.cos(theta_1 - theta_2)**2)
+
+    # @staticmethod
+    def findThetaDoubleDot_2(self, theta_1, theta_2, thetaDot_1, thetaDot_2):
+        return (self.m_2*self.l_2*thetaDot_2**2*np.sin(theta_1 - theta_2)*np.cos(theta_1 - theta_2) + (self.m_1 + self.m_2)*self.g*np.sin(theta_1)*np.cos(theta_1 - theta_2) + self.l_1*thetaDot_1**2*np.sin(theta_1-theta_2)*(self.m_1 + self.m_2) - self.g*np.sin(theta_2)*(self.m_1 + self.m_2)) / (self.l_1*(self.m_1 + self.m_2) - self.m_2*self.l_2*np.cos(theta_1 - theta_2)**2)
+
+
+    def symplecticEulerOneStep(self):
+    # def findThetaDoubleDot_1(theta_1, theta_2, thetaDot_1, thetaDot_2):
+    #     return (-m_2*l_1*thetaDot_1**2*np.sin(theta_1 - theta_2)*np.cos(theta_1 - theta_2) + m_2*g*np.sin(theta_2)*np.cos(theta_1 - theta_2) - m_2*l_2*thetaDot_2**2*np.sin(theta_1 - theta_2) - (m_1 + m_2)*g*np.sin(theta_1)) / ((m_1 + m_2)*l_1 - m_2*l_1*np.cos(theta_1 - theta_2)**2)
+
+    # def findThetaDoubleDot_2(theta_1, theta_2, thetaDot_1, thetaDot_2):
+    #     return (m_2*l_2*thetaDot_2**2*np.sin(theta_1 - theta_2)*np.cos(theta_1 - theta_2) + (m_1 + m_2)*g*np.sin(theta_1)*np.cos(theta_1 - theta_2) + l_1*thetaDot_1**2*np.sin(theta_1-theta_2)*(m_1 + m_2) - g*np.sin(theta_2)*(m_1 + m_2)) / (l_1*(m_1 + m_2) - m_2*l_2*np.cos(theta_1 - theta_2)**2)
+
+        self.u_vector[2] += self.findThetaDoubleDot_1(self.u_vector[0], self.u_vector[1], self.u_vector[2], self.u_vector[3]) * self.deltaT
+        self.u_vector[3] += self.findThetaDoubleDot_2(self.u_vector[0], self.u_vector[1], self.u_vector[2], self.u_vector[3]) * self.deltaT
+
+        self.u_vector[0] += self.u_vector[2] * self.deltaT
+        self.u_vector[1] += self.u_vector[3] * self.deltaT
+
+        self.u_vectorTimeSnapshots.append(copy.deepcopy(self.u_vector))
+
+
+
+    def getAxisCoordinatesOverTimeForParticle(self, _particle, _axis):
         line = []
         if _axis == 2:
-            for i in range(len(u_vectorTimeSnapshots)):
+            for i in range(len(self.u_vectorTimeSnapshots)):
                 line.append(0)
             return line
         if _axis == 0 and _particle == 0:
-            for i in range(len(u_vectorTimeSnapshots)):
-                line.append(l_1*np.sin(u_vectorTimeSnapshots[i][0]))
+            for i in range(len(self.u_vectorTimeSnapshots)):
+                line.append(self.l_1*np.sin(self.u_vectorTimeSnapshots[i][0]))
             return line
         if _axis == 1 and _particle == 0:
-            for i in range(len(u_vectorTimeSnapshots)):
-                line.append(-l_1*np.cos(u_vectorTimeSnapshots[i][0]))
+            for i in range(len(self.u_vectorTimeSnapshots)):
+                line.append(-self.l_1*np.cos(self.u_vectorTimeSnapshots[i][0]))
             return line
         if _axis == 0 and _particle == 1:
-            for i in range(len(u_vectorTimeSnapshots)):
-                line.append(l_1*np.sin(u_vectorTimeSnapshots[i][0]) + l_1*np.sin(u_vectorTimeSnapshots[i][1]))
+            for i in range(len(self.u_vectorTimeSnapshots)):
+                line.append(self.l_1*np.sin(self.u_vectorTimeSnapshots[i][0]) + self.l_1*np.sin(self.u_vectorTimeSnapshots[i][1]))
             return line
         if _axis == 1 and _particle == 1:
-            for i in range(len(u_vectorTimeSnapshots)):
-                line.append(-l_1*np.cos(u_vectorTimeSnapshots[i][0]) - l_1*np.cos(u_vectorTimeSnapshots[i][1]))
+            for i in range(len(self.u_vectorTimeSnapshots)):
+                line.append(-self.l_1*np.cos(self.u_vectorTimeSnapshots[i][0]) - self.l_1*np.cos(self.u_vectorTimeSnapshots[i][1]))
             return line
 
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-ax.set_xlim3d(-radiusOfGraphAxises, radiusOfGraphAxises)
-ax.set_ylim3d(-radiusOfGraphAxises, radiusOfGraphAxises)
-ax.set_zlim3d(-radiusOfGraphAxises + radiusOfGraphAxises*0.23, radiusOfGraphAxises - radiusOfGraphAxises*0.23)
-ax.view_init(40, -5)
-ax.set_title('Double Pendulum')
-ax.set_facecolor(backgroundColorRGB)
-fig.patch.set_facecolor(backgroundColorRGB)
-plt.axis('off')
+    def create_figure(self):
+        fig = plt.figure()
+        self.ax = plt.axes(projection='3d')
+        self.ax.set_xlim3d(-self.radiusOfGraphAxises, self.radiusOfGraphAxises)
+        self.ax.set_ylim3d(-self.radiusOfGraphAxises, self.radiusOfGraphAxises)
+        self.ax.set_zlim3d(-self.radiusOfGraphAxises + self.radiusOfGraphAxises*0.23, self.radiusOfGraphAxises - self.radiusOfGraphAxises*0.23)
+        self.ax.view_init(40, -5)
+        self.ax.set_title('Double Pendulum')
+        self.ax.set_facecolor(self.backgroundColorRGB)
+        fig.patch.set_facecolor(self.backgroundColorRGB)
+        plt.axis('off')
 
-line0, = ax.plot(0, 0, 0, lw=0.5, color=((0, 0, 1)))
-line1, = ax.plot(0, 0, 0, lw=0.5, color=((1, 0, 0)))
-bar0, = ax.plot(0, 0, 0, lw=3, color=(lineColor))
-bar1, = ax.plot(0, 0, 0, lw=3, color=(lineColor))
+        self.line0, = self.ax.plot(0, 0, 0, lw=0.5, color=((0, 0, 1)))
+        self.line1, = self.ax.plot(0, 0, 0, lw=0.5, color=((1, 0, 0)))
+        self.bar0, = self.ax.plot(0, 0, 0, lw=3, color=(self.lineColor))
+        self.bar1, = self.ax.plot(0, 0, 0, lw=3, color=(self.lineColor))
 
-scats = []
-starttime = datetime.now()
-def animation_frame(i):  
+        return fig, self.ax
 
-    ax.view_init(0, -90)
+    def animation_frame(self, scatter_points=False, savefig=False):  
+        '''Creates animation frame for gif'''
+        # scats = []
+        starttime = datetime.now()
+        # f, self.ax = self.create_figure()
+        self.ax.view_init(0, -90)
 
-    for x in range(stepsPerFrame):
-        symplecticEulerOneStep()
+        for x in range(self.stepsPerFrame):
+            self.symplecticEulerOneStep()
 
-    #averageFps = round(i / ((datetime.now() - starttime).total_seconds()))
-    #ax.set_title(f'average fps: {averageFps}')
-    ax.set_title('Double Pendulum')
+        #averageFps = round(i / ((datetime.now() - starttime).total_seconds()))
+        #self.ax.set_title(f'average fps: {averageFps}')
+        self.ax.set_title('Double Pendulum')
 
-    line0.set_data(getAxisCoordinatesOverTimeForParticle(0, 0), getAxisCoordinatesOverTimeForParticle(0, 2))
-    line0.set_3d_properties(getAxisCoordinatesOverTimeForParticle(0, 1))
+        # self.line0.set_data(self.getAxisCoordinatesOverTimeForParticle(0, 0), self.getAxisCoordinatesOverTimeForParticle(0, 2))
+        # self.line0.set_3d_properties(self.getAxisCoordinatesOverTimeForParticle(0, 1))
 
-    line1.set_data(getAxisCoordinatesOverTimeForParticle(1, 0), getAxisCoordinatesOverTimeForParticle(1, 2))
-    line1.set_3d_properties(getAxisCoordinatesOverTimeForParticle(1, 1))
+        self.line1.set_data(self.getAxisCoordinatesOverTimeForParticle(1, 0), self.getAxisCoordinatesOverTimeForParticle(1, 2))
+        self.line1.set_3d_properties(self.getAxisCoordinatesOverTimeForParticle(1, 1))
 
-    bar0.set_data([0, l_1*np.sin(u_vector[0])], [0, 0])
-    bar0.set_3d_properties([0, -l_1*np.cos(u_vector[0])])
+        # self.bar0.set_data([0, self.l_1*np.sin(self.u_vector[0])], [0, 0])
+        # self.bar0.set_3d_properties([0, -self.l_1*np.cos(self.u_vector[0])])
 
-    bar1.set_data([l_1*np.sin(u_vector[0]), l_1*np.sin(u_vector[0]) + l_1*np.sin(u_vector[1])], [0, 0])
-    bar1.set_3d_properties([-l_1*np.cos(u_vector[0]), -l_1*np.cos(u_vector[0]) - l_1*np.cos(u_vector[1])])
+        # self.bar1.set_data([self.l_1*np.sin(self.u_vector[0]), self.l_1*np.sin(self.u_vector[0]) + self.l_1*np.sin(self.u_vector[1])], [0, 0])
+        # self.bar1.set_3d_properties([-self.l_1*np.cos(self.u_vector[0]), -self.l_1*np.cos(self.u_vector[0]) - self.l_1*np.cos(self.u_vector[1])])
 
-    global scats
-    # first remove all old scatters
-    for scat in scats:
-        scat.remove()
-    scats = []
+        # if scatter_points:
+        global scats
+        scats = []
+        # first remove all old scatters
+        for scat in scats:
+            scat.remove()
+        scats = []
 
-    scats.append((ax.scatter(0, 0, 0, color=(0, 0, 0), s=3)))
-    scats.append((ax.scatter(l_1*np.sin(u_vector[0]), 0, -l_1*np.cos(u_vector[0]), color=(0, 0, 1), s=10)))
-    scats.append((ax.scatter(l_1*np.sin(u_vector[0]) + l_1*np.sin(u_vector[1]), 0, -l_1*np.cos(u_vector[0]) - l_1*np.cos(u_vector[1]), color=(1, 0, 0), s=10)))
+        # if scatter pointf at each frame desired
+        # scats.append((self.ax.scatter(0, 0, 0, color=(0, 0, 0), s=3)))
+        # scats.append((self.ax.scatter(self.l_1*np.sin(self.u_vector[0]), 0, -self.l_1*np.cos(self.u_vector[0]), color=(0, 0, 1), s=10)))
+        # scats.append((self.ax.scatter(self.l_1*np.sin(self.u_vector[0]) + self.l_1*np.sin(self.u_vector[1]), 0, -self.l_1*np.cos(self.u_vector[0]) - self.l_1*np.cos(self.u_vector[1]), color=(1, 0, 0), s=10)))
 
-ani = animation.FuncAnimation(fig, func=animation_frame, frames=np.arange(0, 750, 1), interval=milliSecondDelayBetweenFrames)
-plt.show()
+        print('Creating animation....')
+
+
+    
+
+    # return ani
+         
+
+
+
+if __name__ == "__main__":
+    d = DoublePendulum()
+    # a = d.animation_frame(True)   
+    # plt.show()
 
 #ani.save('doublePendulum.gif')
